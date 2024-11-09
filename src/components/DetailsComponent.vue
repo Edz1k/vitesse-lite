d58342cd8ef7133b7df4725c154e1b77ee4a5ad5
Eduard <shartner2006@mail.ru>
2024-11-09 09:15:04 +0500

    changes/footer

--- src/components/DetailsComponent.vue
+++ src/components/DetailsComponent.vue
@@ -14,27 +14,57 @@ useStorage('details', details)
   <div class="p-5">
     <div class="detailsBlock">
       <h1>ФИО</h1>
-      <input v-model="details.name" type="text" class="outline-none" :placeholder="details.name ? '' : 'Введите имя'">
+      <input
+        v-model="details.name"
+        type="text"
+        class="outline-none"
+        :placeholder="details.name ? '' : 'Введите имя'"
+      >
     </div>
     <div class="detailsBlock">
       <h1>ИИН</h1>
-      <input v-model="details.iin" type="text" class="outline-none" :placeholder="details.iin ? '' : 'Введите ИИН'">
+      <input
+        v-model="details.iin"
+        type="text"
+        class="outline-none"
+        :placeholder="details.iin ? '' : 'Введите ИИН'"
+      >
     </div>
     <div class="detailsBlock">
       <h1>Дата рождения</h1>
-      <input v-model="details.date" type="text" class="outline-none" :placeholder="details.date ? '' : 'Введите Дату рождения'">
+      <input
+        v-model="details.date"
+        type="text"
+        class="outline-none"
+        :placeholder="details.date ? '' : 'Введите Дату рождения'"
+      >
     </div>
     <div class="detailsBlock">
       <h1>Номер документа</h1>
-      <input v-model="details.documentNumber" type="text" class="outline-none" :placeholder="details.documentNumber ? '' : 'Введите номер документа'">
+      <input
+        v-model="details.documentNumber"
+        type="text"
+        class="outline-none"
+        :placeholder="details.documentNumber ? '' : 'Введите номер документа'"
+      >
     </div>
     <div class="detailsBlock">
       <h1>Дата выдачи</h1>
-      <input v-model="details.dayRecieve" type="text" class="outline-none" :placeholder="details.dayRecieve ? '' : 'Введите дату выдачи'">
+      <input
+        v-model="details.dayRecieve"
+        type="text"
+        class="outline-none"
+        :placeholder="details.dayRecieve ? '' : 'Введите дату выдачи'"
+      >
     </div>
     <div class="detailsBlock">
       <h1>Срок действия</h1>
-      <input v-model="details.expiring" type="text" class="outline-none" :placeholder="details.expiring ? '' : 'Введите срок действия'">
+      <input
+        v-model="details.expiring"
+        type="text"
+        class="outline-none"
+        :placeholder="details.expiring ? '' : 'Введите срок действия'"
+      >
     </div>
   </div>
 </template>
