d0a4775072be732080dae380e1b2468f1780a90f
Eduard <shartner2006@mail.ru>
2024-11-12 23:45:01 +0500

    chagnges to watch

--- src/components/kaspiCard.vue
+++ src/components/kaspiCard.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ETab } from '~/types/enums'
+import FooterComponent from './FooterComponent.vue'
 
 const props = defineProps({
   modelValue: {
@@ -25,25 +26,32 @@ function handleInput() {
 
 <template>
   <div class="flex flex-col">
-    <div v-if="props.modelValue === ETab.Documents">
-      <div v-if="!photo" class="h-[70svh] flex items-center justify-center">
-        <label for="file-upload" class="border border-coolGray rounded-lg p-3">Загрузите фотографию</label>
-        <input
-          id="file-upload"
-          ref="fileInput"
-          type="file"
-          accept="image/png, image/jpeg"
-          class="hidden"
-          @change="handleInput"
-        >
+    <div class="flex snap-x snap-mandatory overflow-x-auto">
+      <div
+        v-if="props.modelValue === ETab.Documents"
+        class="min-w-full snap-center"
+      >
+        <div v-if="!photo" class="h-[70svh] flex items-center justify-center">
+          <label for="file-upload" class="border border-coolGray rounded-lg p-3">Загрузите фотографию</label>
+          <input
+            id="file-upload"
+            ref="fileInput"
+            type="file"
+            accept="image/png, image/jpeg"
+            class="hidden"
+            @change="handleInput"
+          >
+        </div>
+        <div v-if="photo" class="p-6">
+          <img :src="photo" class="m-auto h-[70svh] w-full transform" alt="">
+        </div>
+        <div>
+          <FooterComponent />
+        </div>
       </div>
-      <div v-if="photo">
-        <img :src="photo" class="m-auto h-[70svh] w-full transform" alt="">
+      <div class="min-w-full snap-center">
+        <DetailsComponent />
       </div>
-    </div>
-    <div v-if="props.modelValue === ETab.Requirements">
-      <DetailsComponent />
     </div>
   </div>
-  <FooterComponent v-if="props.modelValue === ETab.Documents" class="mb-5" />
 </template>
