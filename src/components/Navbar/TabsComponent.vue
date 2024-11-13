049e649654d9f3a954886a34bbc17f374969e03d
luvor <veteratorvulp@gmail.com>
2024-11-13 20:32:51 +0500

    fix: scroll tab

--- src/components/Navbar/TabsComponent.vue
+++ src/components/Navbar/TabsComponent.vue
@@ -1,4 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { UseScrollReturn } from '@vueuse/core'
+
+const props = defineProps<{
+  state: UseScrollReturn
+}>()
+
+const emit = defineEmits<{
+  (event: 'select', pos: number): void
+}>()
+
+const btnLeft = computed(() => {
+  return props.state.x.value > window.innerWidth / 2
+    ? (window.innerWidth / 2) - 18
+    : props.state.x.value
+})
+
+function handleSelect(pos: number) {
+  emit('select', pos)
+}
+</script>
 
 <template>
   <div class="kaspiGrey p-4 pb-0">
@@ -7,12 +27,12 @@
     >
       <div
         class="absolute left-0 top-[-3%] z-5 w-1/2 rounded-lg bg-white p-4"
+        :style="{ left: `calc(${btnLeft}px)` }"
       />
-
-      <li class="tab-item-left z-10 w-1/2 p-[4px]">
+      <li class="tab-item-left z-10 w-1/2 p-[4px]" @click="handleSelect(1)">
         Документ
       </li>
-      <li class="tab-item z-10 w-1/2 p-[4px]">
+      <li class="tab-item z-10 w-1/2 p-[4px]" @click="handleSelect(2)">
         Рекизиты
       </li>
     </ul>
--- src/pages/document/id.vue
+++ src/pages/document/id.vue
@@ -2,12 +2,16 @@
 import { useScroll } from '@vueuse/core'
 
 const el = ref<HTMLElement | null>(null)
-const onScroll = useScroll(el)
+const onScroll = useScroll(el, { behavior: 'smooth' })
+
+function scrollToTab(pos: number) {
+  onScroll.x.value = window.innerWidth * (pos - 1)
+}
 </script>
 
 <template>
-  <TabsComponent :state="onScroll" />
-  <div ref="el" class="flex snap-x snap-mandatory overflow-x-auto">
+  <TabsComponent :state="onScroll" @select="scrollToTab" />
+  <div ref="el" class="hidden-scrollbar flex snap-x snap-mandatory overflow-x-auto">
     <div class="min-w-full snap-center">
       <KaspiCard />
       <FooterComponent />
