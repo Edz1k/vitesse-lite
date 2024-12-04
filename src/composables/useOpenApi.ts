62dee404133b09b4524febcfd6dbdefc3a165ee6
Eduard <shartner2006@mail.ru>
2024-12-04 12:48:36 +0500

    ready

--- src/composables/useOpenApi.ts
+++ src/composables/useOpenApi.ts
@@ -4,9 +4,12 @@ const res = ref()
 export function useOpenApi() {
   const analyzedImage = async (url: string) => {
     try {
-      const response = await axios.post('http://localhost:3000/analyse', {
-        url,
-      })
+      const response = await axios.post(
+        'https://silver-dieffenbachia-adee85.netlify.app/.netlify/functions/request',
+        {
+          url,
+        },
+      )
       res.value = response.data.message
     }
     catch (error: any) {
